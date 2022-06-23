5141325c763feb6c39809e313c6d6b5885aad51d
joko19 <jokoslamet459@gmail.com>
2022-06-23 14:30:52 +0700

    navbar

--- resources/views/Template/sidebar.blade.php
+++ resources/views/Template/sidebar.blade.php
@@ -1,21 +1,12 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-        <!-- <img src="{{asset('images/indihome.png')}}" alt="AdminLTE Logo" class="brand-image elevation-3" style="opacity: .8"> -->
-        <!-- <span class="brand-text font-weight-light text-red">Indihome</span> -->
+        <img src="{{asset('images/indihome.png')}}" alt="AdminLTE Logo" class="brand-image elevation-3" style="opacity: .8">
+        <span class="brand-text font-weight-light text-red">Indihome</span>
     </a>
 
     <!-- Sidebar -->
     <div class="sidebar">
-      <!-- Sidebar user panel (optional) -->
-      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-        <div class="image">
-          <!-- <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image"> -->
-        </div>
-        <div class="info">
-          <a href="#" class="d-block">{{auth()->user()->name}}</a>
-        </div>
-      </div>
         <!-- Sidebar Menu -->
         <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
@@ -60,11 +51,6 @@
                 </a>
               </li> -->
                     </ul>
-                </li>
-                <li class="nav-item">
-                    <a href="{{route('logout')}}" class="nav-link">
-                        <p>Logout</p>
-                    </a>
                 </li>
             </ul>
         </nav>
